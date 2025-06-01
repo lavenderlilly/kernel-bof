af2cc85f23d769c924197390bfd447ec620afa06
lavenderlilly <102565174+lavenderlilly@users.noreply.github.com>
2025-06-01 19:12:35 +0900

    Add files via upload

---
+++ for_organizer/qemu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+qemu-system-x86_64 \
+    -m 256M \
+    -kernel ./bzImage \
+    -initrd ./initramfs.cpio.gz \
+    -append "root=/dev/ram rw rdinit=/root/init console=ttyS0 oops=panic panic=1 quiet nokaslr" \
+    -monitor /dev/null \
+    -no-reboot \
+    -nographic \
+    -serial mon:stdio
+
